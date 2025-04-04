47ccac96b06c171995a968c43ff338767d88d9fb
Alex Riveron <riveron91@gmail.com>
2025-04-03 22:34:53 -0400

    add authentication

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,33 +2,67 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useAuth } from "react-oidc-context"
 
 function App() {
   const [count, setCount] = useState(0)
+  const auth = useAuth()
+
+  const signOutRedirect = () => {
+    const clientId = "6rtm3untp0m1geb9cuva1m94t0"
+    const logoutUri = "https://camislab.com/"
+    const cognitoDomain = "https://us-east-1irrs98eac.auth.us-east-1.amazoncognito.com"
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`
+  }
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <>
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+        <div>
+          <pre> Hello: {auth.user?.profile.email} </pre>
+          <pre> ID Token: {auth.user?.id_token} </pre>
+          <pre> Access Token: {auth.user?.access_token} </pre>
+          <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+          <button onClick={() => auth.removeUser()}>Sign out</button>
+        </div>
+      </>
+    )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
+    </div>
   )
 }
 
--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AuthProvider } from 'react-oidc-context'
+
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_Irrs98eac",
+  client_id: "6rtm3untp0m1geb9cuva1m94t0",
+  redirect_uri: "https://camislab.com/",
+  response_type: "code",
+  scope: "openid email",
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider { ...cognitoAuthConfig }>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
